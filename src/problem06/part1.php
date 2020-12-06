0a38c417c35cadf8503082f43bea1ae996c7a702
Manh Tan Nguyen <tan.mng90@gmail.com>
2020-12-06 00:12:25 -0500

    Complete day 6 of 2020

---
+++ src/problem06/part1.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * part1.php
+ *
+ * Main program for Advent calendar 2020 problem 06
+ * Part 1
+ */
+
+
+// Script constants
+const INPUT_FILE = 'input.txt';
+
+$all_answers = [];
+$result = 0;
+// Open the input file and read the numbers
+$handle = @fopen(INPUT_FILE, "r");
+// A buffer of all the  current lines for the curernt passport
+$current_lines = [];
+if (!$handle) {
+    // Failed to open the file
+    throw new Exception ('Failed opening input file '.INPUT_FILE);
+} else {
+    while (($buffer = fgets($handle)) !== false) {
+        // Clean up the string
+        $input = trim($buffer);
+
+        if (strlen($input)) {
+            // Not an empty line
+            $current_lines[] = $input;
+        } else {
+            // Empty line - Conclude here
+            $group_member_count = count($current_lines);
+            $current_group_answer = implode('', $current_lines);
+
+            // Analyze it
+            $question_frequency = array_count_values(str_split($current_group_answer));
+
+            $current_count = count($question_frequency);
+
+            // print($current_group_answer.': '.$current_count."\n");
+
+            $result += $current_count;
+
+            // Refresh buffer
+            $current_lines = [];
+        }
+    }
+    if (!feof($handle)) {
+         print('Finished reading the input file');
+    }
+}
+
+print('Answer to part 1: '.$result."\n");
