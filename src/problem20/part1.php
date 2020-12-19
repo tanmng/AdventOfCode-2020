ea091be1af212a164d3e01e459806f25f82400ce
Manh Tan Nguyen <tan.mng90@gmail.com>
2020-12-19 01:12:11 -0500

    Prep for problem 20

---
+++ src/problem20/part1.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * part1.php
+ *
+ * Main program for Advent calendar 2020 problem 20
+ * Part 1
+ */
+
+
+const INPUT_FILE = 'sample.txt';
+
+// Open the input file and read the numbers
+$handle = @fopen(INPUT_FILE, "r");
+if (!$handle) {
+    // Failed to open the file
+    throw new Exception ('Failed opening input file '.INPUT_FILE);
+} else {
+    while (($buffer = fgets($handle)) !== false) {
+        // Clean up the string
+        $input = trim($buffer);
+    }
+    if (!feof($handle)) {
+         print('Finished reading the input file');
+    }
+}
+
+print('Answer to part 1: '.null."\n");
